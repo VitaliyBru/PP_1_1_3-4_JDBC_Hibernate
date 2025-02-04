d77a7b390323a2e66e34abd50b007a81eaa8c853
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-02-04 22:10:08 +0300

    Добавляет в класс Util метод getConnection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbc_test";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "kataacademy";
+    private static Connection connection;
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
