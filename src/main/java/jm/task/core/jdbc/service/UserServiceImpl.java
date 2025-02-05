2b07898d61d6f590f7663c71dd4f86bf84cefc95
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-02-05 13:06:20 +0300

    Добавляет реализацию методов в UserServiceImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,103 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
+        String sql = """
+                    CREATE TABLE IF NOT EXISTS `users` (
+                      `id` BIGINT NOT NULL AUTO_INCREMENT,
+                      `name` VARCHAR(45) NOT NULL,
+                      `lastName` VARCHAR(45) NOT NULL,
+                      `age` TINYINT(1) NOT NULL,
+                      PRIMARY KEY (`id`))
+                    ENGINE = InnoDB""";
 
+        try (Connection connection = Util.getConnection();
+             Statement stmt = connection.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS `jdbc_test`.`users`";
+        try (Connection connection = Util.getConnection();
+             Statement stmt = connection.createStatement()) {
+
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "INSERT INTO `jdbc_test`.`users` (name, lastName, age) VALUES (?, ?, ?)";
+        try (Connection connection = Util.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
 
+            pstmt.setString(1, name);
+            pstmt.setString(2, lastName);
+            pstmt.setByte(3, age);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "DELETE FROM `jdbc_test`.`users` WHERE id = ?";
+
+        try (Connection connection = Util.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            pstmt.setLong(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM `jdbc_test`.`users`";
+        List<User> allUsers = new ArrayList<>();
+        User user;
+        ResultSet resultSet;
+
+        try (Connection connection = Util.getConnection();
+             Statement stmt = connection.createStatement()) {
+
+            resultSet = stmt.executeQuery(sql);
+            while (resultSet.next()) {
+                user = new User(resultSet.getString("name"), resultSet.getString("lastName"),
+                        resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                allUsers.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return allUsers;
     }
 
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE `jdbc_test`.`users`";
 
+        try (Connection connection = Util.getConnection();
+             Statement stmt = connection.createStatement()) {
+
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
