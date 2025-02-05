87afef7211f03deb2b120b0ed538e7ad7aed2306
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-02-05 13:05:00 +0300

    Убирает паттерн singleton

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,17 +8,12 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc_test";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "kataacademy";
-    private static Connection connection;
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         try {
-            if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-            }
+            return DriverManager.getConnection(URL, USER_NAME, PASSWORD);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-
-        return connection;
     }
 }
