490fe0179681d03ea23c592b862e53c68d91e98d
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-02-11 12:10:04 +0300

    Добавляет метод для получения sessionFactory

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc_test";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "kataacademy";
+    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         try {
@@ -15,5 +25,28 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            sessionFactory = buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        Properties properties = new Properties();
+
+        properties.put(Environment.DRIVER, DRIVER);
+        properties.put(Environment.URL, URL);
+        properties.put(Environment.USER, USER_NAME);
+        properties.put(Environment.PASS, PASSWORD);
+        properties.put(Environment.DIALECT, DIALECT);
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(Environment.SHOW_SQL, "true");
+        configuration.setProperties(properties);
+
+        return configuration.buildSessionFactory();
     }
 }
