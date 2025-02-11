aa7459ac5a77c43fe766484a01f8da77acde6f93
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-02-11 18:10:39 +0300

    Добавляет реализацию метода main класса Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,11 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoHibernateImpl userService = new UserDaoHibernateImpl();
 
         // Создание таблицы в БД
         userService.createUsersTable();
@@ -27,5 +28,8 @@ public class Main {
 
         // Удаление таблицы
         userService.dropUsersTable();
+
+        // Закрытие sessionFactory
+        Util.getSessionFactory().close();
     }
 }
