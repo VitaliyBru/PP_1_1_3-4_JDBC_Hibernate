47e69bb6411c513da8b567ea972824acbe0fc89c
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-02-11 12:11:35 +0300

    Добавляет методы для создания и удаления таблицы users

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,26 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final String SQL_CREATE_TABLE = """
+                    CREATE TABLE IF NOT EXISTS `jdbc_test`.`users` (
+                      `id` BIGINT NOT NULL AUTO_INCREMENT,
+                      `name` VARCHAR(45) NOT NULL,
+                      `lastName` VARCHAR(45) NOT NULL,
+                      `age` TINYINT(1) NOT NULL,
+                      PRIMARY KEY (`id`))
+                    ENGINE = InnoDB""";
+    private static final String SQL_DROP_TABLE = "DROP TABLE IF EXISTS `jdbc_test`.`users`";
+
+    private final SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,12 +28,26 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery(SQL_CREATE_TABLE).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery(SQL_DROP_TABLE).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
